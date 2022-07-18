89ebd3e787370d51774fab220a8b95e7f4ab3ec4
RinaKorca <rk48713@ubt-uni.net>
2022-07-18 22:45:53 +0200

    second commit

---
+++ PortfolioRK/Database/AppContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PortfolioRK.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioRK.Database
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+        }
+
+ 
+            public DbSet<Repository> Repositories { get; set; }
+        
+    }
+}
---
+++ PortfolioRK/Model/Repository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PortfolioRK.Model
+{
+    public class Repository
+    {
+        [Key]
+        public int RepositoryId { get; set; }
+
+        public String RepoName { get; set; }
+
+        public String RepoDescription { get; set; }
+
+        public String Language { get; set; }
+    }
+}
